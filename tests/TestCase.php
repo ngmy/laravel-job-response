30acb0ed3153adc5654cf4048a167d3f9325e495
Yuta Nagamiya <y.nagamiya@gmail.com>
2023-06-29 23:58:46 +0900

    Enable Testbench auto discovery globally

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,6 @@ namespace Williamjulianvicary\LaravelJobResponse\Tests;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Williamjulianvicary\LaravelJobResponse\LaravelJobResponseServiceProvider;
 
 /**
  * @internal
@@ -17,22 +16,12 @@ abstract class TestCase extends OrchestraTestCase
 {
     use DatabaseTransactions;
 
+    protected $enablesPackageDiscoveries = true;
+
     protected function setUp(): void
     {
         parent::setUp();
         \assert(false !== realpath(__DIR__.'/database/migrations'));
         $this->loadMigrationsFrom(realpath(__DIR__.'/database/migrations'));
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     *
-     * @param mixed $app
-     */
-    protected function getPackageProviders($app): array
-    {
-        return [
-            LaravelJobResponseServiceProvider::class,
-        ];
     }
 }
