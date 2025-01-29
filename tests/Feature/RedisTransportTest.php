e58b6db608976dd68e3f0d46fcabf5c340134386
Yuta Nagamiya <y.nagamiya@gmail.com>
2025-01-29 17:02:05 +0900

    Refactor tests to PHPUnit 10 style

--- tests/Feature/RedisTransportTest.php
+++ tests/Feature/RedisTransportTest.php
@@ -9,32 +9,41 @@ use Illuminate\Contracts\Bus\Dispatcher;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Config;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Williamjulianvicary\LaravelJobResponse\CanRespond;
+use Williamjulianvicary\LaravelJobResponse\ExceptionResponse;
+use Williamjulianvicary\LaravelJobResponse\Exceptions\JobFailedException;
 use Williamjulianvicary\LaravelJobResponse\Exceptions\TimeoutException;
 use Williamjulianvicary\LaravelJobResponse\Facades\LaravelJobResponse;
+use Williamjulianvicary\LaravelJobResponse\LaravelJobResponseServiceProvider;
 use Williamjulianvicary\LaravelJobResponse\Response;
 use Williamjulianvicary\LaravelJobResponse\ResponseCollection;
+use Williamjulianvicary\LaravelJobResponse\ResponseFactory;
 use Williamjulianvicary\LaravelJobResponse\Tests\Data\TestJob;
 use Williamjulianvicary\LaravelJobResponse\Tests\TestCase;
+use Williamjulianvicary\LaravelJobResponse\Transport\CacheTransport;
+use Williamjulianvicary\LaravelJobResponse\Transport\RedisTransport;
+use Williamjulianvicary\LaravelJobResponse\Transport\TransportAbstract;
 use Williamjulianvicary\LaravelJobResponse\Transport\TransportContract;
+use Williamjulianvicary\LaravelJobResponse\TransportFactory;
 
 /**
  * @internal
- *
- * @covers \Williamjulianvicary\LaravelJobResponse\CanRespond
- * @covers \Williamjulianvicary\LaravelJobResponse\ExceptionResponse
- * @covers \Williamjulianvicary\LaravelJobResponse\Exceptions\JobFailedException
- * @covers \Williamjulianvicary\LaravelJobResponse\Exceptions\TimeoutException
- * @covers \Williamjulianvicary\LaravelJobResponse\Facades\LaravelJobResponse
- * @covers \Williamjulianvicary\LaravelJobResponse\LaravelJobResponse
- * @covers \Williamjulianvicary\LaravelJobResponse\LaravelJobResponseServiceProvider
- * @covers \Williamjulianvicary\LaravelJobResponse\Response
- * @covers \Williamjulianvicary\LaravelJobResponse\ResponseCollection
- * @covers \Williamjulianvicary\LaravelJobResponse\ResponseFactory
- * @covers \Williamjulianvicary\LaravelJobResponse\Transport\CacheTransport
- * @covers \Williamjulianvicary\LaravelJobResponse\Transport\RedisTransport
- * @covers \Williamjulianvicary\LaravelJobResponse\Transport\TransportAbstract
- * @covers \Williamjulianvicary\LaravelJobResponse\TransportFactory
  */
+#[CoversClass(CanRespond::class)]
+#[CoversClass(ExceptionResponse::class)]
+#[CoversClass(JobFailedException::class)]
+#[CoversClass(TimeoutException::class)]
+#[CoversClass(LaravelJobResponse::class)]
+#[CoversClass(\Williamjulianvicary\LaravelJobResponse\LaravelJobResponse::class)]
+#[CoversClass(LaravelJobResponseServiceProvider::class)]
+#[CoversClass(Response::class)]
+#[CoversClass(ResponseCollection::class)]
+#[CoversClass(ResponseFactory::class)]
+#[CoversClass(CacheTransport::class)]
+#[CoversClass(RedisTransport::class)]
+#[CoversClass(TransportAbstract::class)]
+#[CoversClass(TransportFactory::class)]
 final class RedisTransportTest extends TestCase
 {
     public function testJobTransportSuccess(): void
